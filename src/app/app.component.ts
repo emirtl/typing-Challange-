fd690ada3c7f7af0fb3595bb18d594d32183a6b3
hesam <tlemir55@gmail.com>
2023-12-13 14:39:00 +0300

    typing chalange done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+//faker
+import { faker } from '@faker-js/faker';
+//primeNg
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, CardModule,InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+ randomText=faker.lorem.sentence()
+  enteredtext=''
+
+
+  handleEvent(value:string) {
+    this.enteredtext=value
+  }
+
+  compare(letter: string, enteredLetter: string) {
+    if(!enteredLetter){
+      return 'pending'
+    }
+    return letter===enteredLetter?'correct':'incorrect'
+  }
 }
